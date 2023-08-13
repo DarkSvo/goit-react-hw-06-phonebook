9efce5d6d5f16b1bb8580600a627d474b8530035
Anton Hodorlov <95712062+DarkSvo@users.noreply.github.com>
2023-08-13 21:26:03 +0300

    1

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import { Container, Title, ContactListTitle } from './App/App.styled';
+import Form from './Form/Form';
+import ContactsList from './Contact__List/Contact__List';
+import Filter from './Filter/Filter';
+
+const App = () => {
+  return (
+    <Container>
+      <Title>My Phonebook</Title>
+      <Form />
+      <ContactListTitle> My contacts list</ContactListTitle>
+      <Filter />
+      <ContactsList />
+    </Container>
+  );
+};
+
+export default App;
